3089f7b0484ee8440a9f10c61ab15d7704ec9a22
rockpicado <81575252+rockpicado@users.noreply.github.com>
2023-12-05 10:16:18 +0000

    Show hanja ruby text

--- pages/api/param.tsx
+++ pages/api/param.tsx
@@ -1,5 +1,6 @@
 import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
+import hanja from 'hanja'
 
 export const config = {
   runtime: 'edge',
@@ -30,6 +31,7 @@ export default function handler(req: NextRequest) {
       textsizeClass = 'text-8xl'
     }
     
+    const hanjaRegex = /[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]/;
 
     return new ImageResponse(
       (
@@ -50,10 +52,22 @@ export default function handler(req: NextRequest) {
 >
   <div tw="flex flex-col w-full h-full items-center justify-center">
     <div tw="flex w-full">
-      <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-        <div tw={`${textsizeClass} leading-relaxed`}>
-              {title}
-        </div>
+      <div tw="flex flex-row w-full py-12 px-4 items-center justify-between p-8">
+        <p tw={`${textsizeClass} flex leading-relaxed flex-wrap justify-center`}>
+              {title.split(' ').map((v, k) => {
+                return (<>
+                  <span style={{ whiteSpace: 'pre' }}>{hanja.split(v).map((hSegment) => {
+                    return (<>
+                      { !hanjaRegex.test(hSegment) ?
+                        (<span>{hSegment}</span>) :
+                        (<ruby tw='flex max-w'>{hSegment}<rt tw='absolute text-xl w-full justify-center -mt-1'>{hanja.translate(hSegment, hanja.TRANSLATE_TYPES.SUBSTITUTION)}</rt></ruby>)
+                      }
+                    </>);
+                  })} </span>
+
+                </>);
+          })}
+        </p>
       </div>
     </div>
   </div>
